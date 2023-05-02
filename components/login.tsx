238f0adb23e0514e6232a498052c783d819e0cf3
91Pudge <joshhaley91@gmail.com>
2023-05-02 21:20:35 +0100

    Feat: Create login page

--- components/login.tsx
+++ components/login.tsx
@@ -1,5 +1,25 @@
-const login = () => {
-  return <>hi</>;
+import styles from "../styles/login.module.css";
+
+const Login = () => {
+  return (
+    <>
+      <div className={styles["container"]}>
+        <form id="login">
+          <div className={styles["form-group"]}>
+            <label htmlFor={"username"}>Username</label>
+            <input type="text" id={"username"} name="username"></input>
+          </div>
+          <div className={styles["form-group"]}>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password"></input>
+          </div>
+          <div className={styles["buttonHolder"]}>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
-export default login;
+export default Login;
