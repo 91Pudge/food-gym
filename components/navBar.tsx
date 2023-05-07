8a6901714dd834e31e3064d15aee82329c136f0a
91Pudge <joshhaley91@gmail.com>
2023-05-07 09:09:21 +0100

    Feat: Add login buttons to navbar

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import styles from "../styles/navBar.module.css";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const NavBar = () => {
+  const { data: session } = useSession();
   return (
     <div className={styles.bar}>
       <h1>Gym food</h1>
@@ -9,6 +11,20 @@ const NavBar = () => {
         <Link href={"/"}>Home</Link>
         <Link href={"/about"}>About</Link>
         <Link href={"/contact"}>Contact</Link>
+        <div className={styles["login"]}>
+          {!session && (
+            <>
+              Not signed in <br />
+              <button onClick={() => signIn()}>Sign in</button>
+            </>
+          )}
+          {session && (
+            <div className={styles["login"]}>
+              Signed in as {session.user?.email} <br />
+              <button onClick={() => signOut()}>Sign out</button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
