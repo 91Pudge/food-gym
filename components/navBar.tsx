784daa6784f4b69a262f06f914d666d92bcbd5f1
91Pudge <joshhaley91@gmail.com>
2023-05-08 18:48:27 +0100

    Chore: Re-arrange search button

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -17,6 +17,7 @@ const NavBar = () => {
     <div className={styles["bar"]}>
       <h1>Gym food </h1>
       <div className={styles["nav"]}>
+        {session && <Link href={"/recipe-page"}>Search</Link>}
         <Link href={"/"}>Home</Link>
         <Link href={"/about"}>About</Link>
         <Link href={"/contact"}>Contact</Link>
