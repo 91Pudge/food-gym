f1613e68e1f157d9393d294a1f27f44ae044bda9
91Pudge <joshhaley91@gmail.com>
2023-05-09 20:44:10 +0100

    Feat: Add data to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,17 @@
+import styles from "../styles/about.module.css";
+
 const About = () => {
-  return <div>hi</div>;
+  return (
+    <div className={styles["container"]}>
+      <p>
+        In order to hone my coding skills, I made the decision to develop a food
+        recipe website called "Food Gym." My goal was to establish a platform
+        that would not only offer recipes, but also promote healthy eating
+        habits, educate users on nutrition, and inspire individuals to explore
+        various cuisines.{" "}
+      </p>
+    </div>
+  );
 };
 
 export default About;
