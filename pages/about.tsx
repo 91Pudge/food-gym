a5219c354fe21b300819ca55e4200406ff49a399
91Pudge <joshhaley91@gmail.com>
2023-05-09 21:25:09 +0100

    Chore: Change to punctuation

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className={styles["container"]}>
       <p>
         In order to hone my coding skills, I made the decision to develop a food
-        recipe website called "Food Gym." My goal was to establish a platform
+        recipe website called "Food Gym". My goal was to establish a platform
         that would not only offer recipes, but also promote healthy eating
         habits, educate users on nutrition, and inspire individuals to explore
         various cuisines.{" "}
