83f5de2c36472e98acf4ba7660b979951992af04
91Pudge <joshhaley91@gmail.com>
2023-05-13 07:23:34 +0100

    Feat: Create API endpoint for fetching user data

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,22 +1,24 @@
 import clientPromise from "../../lib/mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+export const getUsers = async () => {
+
+  const client = await clientPromise;
+  // const db = client.db("food-gym");
+
+  const users = await client
+    .db("food-gym")
+    .collection("users")
+    .find({})
+    //   .sort({ metacritic: -1 })
+    .limit(10)
+    .toArray();
+  return users
+}
 
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const client = await clientPromise;
-    const db = client.db("food-gym");
+  const data = await getUsers()
+  res.status(200).json(JSON.stringify(data))
 
-    const users = await db
-      .collection("users")
-      .find({})
-      //   .sort({ metacritic: -1 })
-      .limit(10)
-      .toArray();
-
-    res.json(users);
-  } catch (e) {
-    console.error(e);
-  }
 };
