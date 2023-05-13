33bc8686bf3ee3f7e31590173a2343bae312691c
91Pudge <joshhaley91@gmail.com>
2023-05-13 21:37:31 +0100

    Feat: Create post api route to store in mongodb

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import clientPromise from "../../lib/mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -8,17 +9,32 @@ export const getUsers = async () => {
 
   const users = await client
     .db("food-gym")
-    .collection("users")
+    .collection("stored-recipe")
     .find({})
-    //   .sort({ metacritic: -1 })
     .limit(10)
     .toArray();
   return users
 }
 
+export const add = async (recipe: any): Promise<ObjectId> => {
+  const client = await clientPromise;
+
+  const response = await client.db("food-gym").collection("stored-recipe").insertOne(recipe)
+
+  return response.insertedId
+
+}
+
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = await getUsers()
-  res.status(200).json(JSON.stringify(data))
+  if (req.method === 'GET') {
+
+    const data = await getUsers()
+    res.status(200).json(JSON.stringify(data))
+  } else if (req.method === 'POST') {
+    console.log(req.body)
+    const insertedId = await add(req.body)
+    res.status(200).json(insertedId)
+  }
 
 };
