887bd0889c2baabc93795454f577ac0ed825daf8
91Pudge <joshhaley91@gmail.com>
2023-05-14 10:00:48 +0100

    Feat: Add api route post to mongodb

--- components/card.tsx
+++ components/card.tsx
@@ -1,3 +1,5 @@
+import { FormEventHandler } from "react";
+import { add } from "../pages/api/user";
 import styles from "../styles/card.module.css";
 
 export interface Recipe {
@@ -32,6 +34,16 @@ export interface Recipe {
 }
 
 const Card = ({ apiData }: any) => {
+  const handleSubmit: FormEventHandler = async (event) => {
+    event.preventDefault();
+    await fetch("/api/user", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ image: "josh", email: "j@j.j" })
+    });
+  };
   console.log(apiData);
   return (
     <div className={styles["cardStyling"]}>
@@ -44,6 +56,9 @@ const Card = ({ apiData }: any) => {
               <p>{meal.recipe.label}</p>
               <a href={meal.recipe.url}>See the full recipe here</a>
             </div>
+            <form onSubmit={handleSubmit}>
+              <button>Store</button>
+            </form>
           </div>
         );
       })}
