f8e0d386150e669109598788e4554ad4eab0ce05
91Pudge <joshhaley91@gmail.com>
2023-05-18 21:23:01 +0100

    Feat: Add footer to page

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/card.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>This is a footers</p>
+    </footer>
+  );
+};
+
+export default Footer;
